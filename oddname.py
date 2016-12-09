779ae7d9837666da01bfbdd876d89dbf871ca5f0
jc450653 <praneeth.mekala@my.jcu.edu.au>
2016-12-09 20:01:00 +1000

    complete the file

---
+++ oddname.py
@@ -0,0 +1,10 @@
+valid = False
+while not valid:
+    name = input("enter your name:")
+    if name != "" and not name.isspace():
+          valid = True
+print(valid)
+
+l = len(name)
+for i in range(0, l, 2):
+    print(name[i])
